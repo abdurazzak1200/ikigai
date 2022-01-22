60f16e8f2d3d6820f289dc1c40ca16630af280b0
abdurazzak1200 <toloev.abi99@gmail.com>
2022-01-23 01:26:51 +0600

    end models Post, Comment

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -11,12 +11,3 @@ class Profile(models.Model):
     def __str__(self):
         return self.user
 
-    def save(self):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField('Заголовок', max_length=200)
+    img = models.ImageField('Фото поста', upload_to='post-image/')
+    description = models.TextField('Описание')
+    likes = models.ManyToManyField(User, related_name="likes")
+    archived = models.BooleanField('Архив', default=False)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} : {self.title}"
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commented_time = models.DateTimeField(auto_now_add=True, auto_now=False)
+    comment = models.CharField('Комментарии', max_length=250)
