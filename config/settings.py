dce67d0d9f391955522da87c7a4207eabf366fe1
abdurazzak1200 <toloev.abi99@gmail.com>
2022-01-27 02:15:43 +0600

    stting heroku 4

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
