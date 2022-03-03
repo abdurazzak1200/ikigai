7e7bd49b98cfb81b7e3b26e9eb59f1b8bdf6e6b5
abdurazzak1200 <toloev.abi99@gmail.com>
2022-03-03 16:36:55 +0600

    change urls in app posts

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
 from .views import *
-from . import views
 
 urlpatterns = [
-    path('', AllPostView.as_view(), name='index'),
-    path('<str:category_slug>/', AllPostView.as_view(), name='category_list'),
     path('category-create/', CategoryCreatedView.as_view(), name='category-create'),
+    path('', AllPostView.as_view(), name='index'),
     path('post-create/', PostCreatedView.as_view(), name='post-create'),
     path('archived-post', AllArchivedPostView.as_view(), name='archived_post'),
     # path('archive_false/<int:pk>', ChandeArchiveFalse.as_view(), name='archived_false'),
     # path('archive_true/<int:pk>', ChandeArchiveTrue.as_view(), name='archived_true'),
+    path('<str:category_slug>/', AllPostView.as_view(), name='category_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='detail_post'),
-    path('<int:pk>/add_post/', AddCommentView.as_view(), name='add_comment'),
-    path('post-edit/<int:pk>/', views.UpdatePost.as_view(), name='edit-post'),
+    path('post-edit/<int:pk>/', UpdatePost.as_view(), name='edit-post'),
     path('post/like/<int:post_id>/', like_post, name="like"),
-
 ]
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('qwerty/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
 
 ]
