ab40cfc52f4c81cc4c6967153b0eebe71dd43a43
abdurazzak1200 <toloev.abi99@gmail.com>
2022-03-17 20:08:07 +0600

    edit static_url in settings

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import  serializers
 from django.contrib.auth.models import User
 from posts.models import Post, Category, Comment
 
+
 class UsersSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
