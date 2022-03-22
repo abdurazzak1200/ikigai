a76fe892ec95a5e5decdd7de15578704cf70d74d
abdurazzak1200 <toloev.abi99@gmail.com>
2022-03-22 15:17:04 +0600

    edit staticfiledirs

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    "/Users/test/Desktop/pinterest/ikigai/static/",
+    BASE_DIR, "static",
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
